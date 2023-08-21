d7b781fdc9b720ead171377262c2085f2866ef50
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2023-08-21 00:02:24 -0500

    Camera rotation added to use the right joystick

--- Scenes/Camera3D.gd
+++ Scenes/Camera3D.gd
@@ -4,14 +4,53 @@ var mouse_direction = Vector2(0,0)
 var initial_camera_rotation_x = rotation.x
 var initial_camera_rotation_y = rotation.y
 
-func _input(event):
+const CAMERA_ROTATION_SPEED= 0.5
+const RETURN_ROTATION_SPEED= 0.1
+
+#func _input(event):
 	
-	if Input.is_action_pressed("FREE_CAMERA"):
-		if event is InputEventMouseMotion:
-			mouse_direction = event.relative
-			rotation.y -= deg_to_rad(mouse_direction.x * 0.1)
-			rotation.x -= deg_to_rad(mouse_direction.y * 0.1)
-		elif Input.is_action_pressed("RESET_CAMERA"):
-			rotation.x = initial_camera_rotation_x
-			rotation.y = initial_camera_rotation_y
-			
+#	if Input.is_action_pressed("FREE_CAMERA"):
+#		if event is InputEventMouseMotion:
+#			mouse_direction = event.relative
+#			rotation.y -= deg_to_rad(mouse_direction.x * 0.1)
+#			rotation.x -= deg_to_rad(mouse_direction.y * 0.1)
+#		elif Input.is_action_pressed("RESET_CAMERA"):
+#			rotation.x = initial_camera_rotation_x
+#			rotation.y = initial_camera_rotation_y
+
+
+func get_input(delta):
+	
+	var is_any_key_pressed = false
+	# Block rotation on the Z axis
+	rotation.z = 0
+	
+	var angle_x = 0
+	if Input.is_action_pressed("CAMERA_RIGHT"):
+		rotate_y(CAMERA_ROTATION_SPEED * delta)
+		is_any_key_pressed = true
+	if Input.is_action_pressed("CAMERA_LEFT"):
+		rotate_y(-CAMERA_ROTATION_SPEED * delta)
+		is_any_key_pressed = true
+	if Input.is_action_pressed("CAMERA_UP"):
+		is_any_key_pressed = true
+		if rad_to_deg(rotation.x) <= 90:
+			rotate_x(-CAMERA_ROTATION_SPEED * delta)
+	if Input.is_action_pressed("CAMERA_DOWN"):	
+		is_any_key_pressed = true
+		if rad_to_deg(rotation.x) <= 0:
+			rotate_x(CAMERA_ROTATION_SPEED * delta)
+	
+	# If no keys are pressed, return to the initial position
+	if not is_any_key_pressed:
+		rotation.x = lerp(rotation.x, initial_camera_rotation_x, RETURN_ROTATION_SPEED)
+		rotation.y = lerp(rotation.y, initial_camera_rotation_y, RETURN_ROTATION_SPEED)
+
+	
+	
+
+	# Your other input handling code can go here...
+
+func _physics_process(delta):
+	
+	get_input(delta)
--- Scenes/hy_drone.gd
+++ Scenes/hy_drone.gd
@@ -7,38 +7,19 @@ extends RigidBody3D
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var water = get_node('/root/World/WaterPlane')
 
-# @onready var probes = $ProbeContainer.get_children()
 @onready var probes = $ProbeContainer.get_children()
-
-#Vehicle controller variables
-
-
-#var horse_power  = 200
-#var accel_speed = 20
-#
-#var steer_angle = deg_to_rad(30)
-#var steer_speed = 3
-#
-#var brake_power = 40
-#var brake_speed = 40
 
 
 #------------USING A RIDIG BODY INSTEAD A VEHICLE NODE
 
 var max_speed = 3.09 #Aprox 6 kn
-
-
 var SPEED = 0.05
-const ROTATION_SPEED = 0.5
-
+const ANGULAR_SPEED = 0.5
 
 #----------Vehicle Varibales
 
 @export var engine_force = 5.80
 var velocity = Vector3.ZERO
-
-var direction = Vector3.FORWARD
-
 
 # Water Effect varibles
 var submerged := false
@@ -47,38 +28,16 @@ var submerged := false
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _physics_process(delta):
 	
-	#Vehicle controller
-	
-	#------------USING A RIDIG BODY INSTEAD A VEHICLE NODE
-#	var inputVector = Vector3.ZERO
-#
-#	var throttle_input = Input.get_action_strength("ui_up")
-#	print(throttle_input)
-	
-#	var throttle_input = -Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down")
-#	engine_force = lerp(engine_force,throttle_input * horse_power, accel_speed * delta)
-#	##
-#	var steer_input = -Input.get_action_strength("ui_right") + Input.get_action_strength("ui_left")
-#	steering = lerp(steering, steer_input * steer_angle, steer_speed * delta)
-#
-#	var brake_input = Input.get_action_strength("BRAKE")
-#	brake = brake_input * brake_power
-	
-	
-	############################
+	# Vehicle control function
 	get_input(delta)
-	move_and_collide(direction)	
-	##############################
+	move_and_collide(velocity)	
 	
-	
-
 	# Bouyancty effect
 	submerged = false
 	for p in probes:
@@ -95,29 +54,20 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 		
 
 func get_input(delta):
-	
-	#################
-	
-	direction = Vector3(Input.get_action_strength("RIGHT") - Input.get_action_strength("LEFT"),
-						 0, 
-						 Input.get_action_strength("REVERSE") - Input.get_action_strength("ACCELERATE"))
-						
-	$HUD/Label.text = "direction: " + str(direction)
-	$HUD/Label2.text = "direction.length(): " + str(direction.length())
 
-	#################S
+	var velocity_y = velocity.y
+	velocity = Vector3.ZERO
 	
+	if Input.is_action_pressed("ACCELERATE"):
+		velocity -=transform.basis.z * SPEED
+	elif Input.is_action_pressed("REVERSE"):
+		velocity += transform.basis.z * SPEED
 	
-#	var velocity_y = velocity.y
-#	velocity = Vector3.ZERO
+	if Input.is_action_pressed("RIGHT"):
+		rotate_y(-ANGULAR_SPEED * delta)
+	elif Input.is_action_pressed("LEFT"):	
+		rotate_y(ANGULAR_SPEED * delta)
 
-#	if Input.is_action_pressed("ACCELERATE"):
-#		velocity -=transform.basis.z * SPEED
-#	if Input.is_action_pressed("REVERSE"):
-#		velocity += transform.basis.z * SPEED
-#	if Input.is_action_pressed("RIGHT"):
-#		rotate_y(-ROTATION_SPEED * delta)
-#	if Input.is_action_pressed("LEFT"):	
-#		rotate_y(ROTATION_SPEED * delta)
 	
-#	velocity.y = velocity_y
+
+	velocity.y = velocity_y
