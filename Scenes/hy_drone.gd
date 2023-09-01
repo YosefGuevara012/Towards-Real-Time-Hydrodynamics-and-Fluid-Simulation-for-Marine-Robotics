60cb6ddec07ae4c3e1e417c94d48a8b9170fe5ac
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2023-09-01 01:58:02 -0500

    I fixed some issue with the force applyed to the vehicle per each probe to simulate the movement of the waves

--- Scenes/hy_drone.gd
+++ Scenes/hy_drone.gd
@@ -4,31 +4,35 @@ extends RigidBody3D
 @export var water_drag := 0.05
 @export var water_angular_drag := 0.1
 
+# Global Physics varibles
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var water = get_node('/root/World/WaterPlane')
 @onready var HUD = get_node('/root/World/HUD')
 
+# Bouyancy variables
+@export_range(1020, 1029) var surface_seawater_density : int =  1020
+@export var object_total_volume  = 0.0271 * 2# The total volume of the object in m^3
+@export_range(0, 1) var submerged_fraction : float =  0.9
 @onready var probes = $ProbeContainer.get_children()
+var submerged := false
 
-# USING A RIDIG BODY INSTEAD A VEHICLE NODE
-@export var object_total_volume = 0.0271 * 2 # m^3, only takes in count both hulls
-
-# max speed in kn to m/s
+# Vehicle mechanics characteristics added
 @export_range(1, 6) var survey_speed : float =  3
-var throttle = 0
+
 const ANGULAR_SPEED = 0.5
-var time = 0
+var throttle = 0
 var velocity = Vector3.ZERO
 
 # Thrusters variables
 var FWD_max_rpm = 3527 * 6 # from rpm to Degrees per second 360/60 for 16 V
 var BWD_max_rpm = 3465 * 6 # from rpm to Degrees per second 360/60 for 16 V
-# Water Effect varibles
-var submerged := false
+
 
 # Define a signal for updating the velocity
 signal vehicle_velocity(new_velocity: Vector3)
 
+# sincronization variable:
+var time = 0
 
 func _physics_process(delta):
 	
@@ -43,13 +47,22 @@ func _physics_process(delta):
 	bouyancy()
 
 func bouyancy():
-	# Bouyancty effect
+	# Optimization: Check if object's central point is above water (you can adjust this based on your needs)
+	var central_depth = water.get_height(global_position) - global_position.y
+	if central_depth <= 0:
+		return  # Skip checking the probes if the object is above water.
+	
+	# Bounyancy variables	
 	submerged = false
+	var probe_container_weight = 1.0 / probes.size()
+	var displaced_volume = object_total_volume * submerged_fraction * probe_container_weight
+	var buoyancy_force = surface_seawater_density * displaced_volume * gravity
+	
 	for p in probes:
-		var depth = water.get_height(p.global_position) - p.global_position.y 
+		var depth = water.get_height(p.global_position) - p.global_position.y
 		if depth > 0:
 			submerged = true
-			apply_force(Vector3.UP * float_force * gravity * depth, p.global_position - global_position)
+			apply_force(Vector3.UP * buoyancy_force,  p.global_position - global_position)
 
 func _integrate_forces(state: PhysicsDirectBodyState3D):
 	
--- Scenes/nomad_filter.gd
+++ Scenes/nomad_filter.gd
@@ -1,24 +1,18 @@
 extends RigidBody3D
 
-
-@export var float_force := 1.0
 @export var water_drag := 0.05
 @export var water_angular_drag := 0.05
-@export_range(1020, 1029) var surface_seawater_density : int =  1020
 
+# Global Physics varibles
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var water = get_node('/root/World/WaterPlane')
 
-
+# Bouyancy variables
+@export_range(1020, 1029) var surface_seawater_density : int =  1020
 @export var object_total_volume  = 0.004 # The total volume of the object in m^3
 @export_range(0, 1) var submerged_fraction : float =  0.85
-
-#---------------- Added to calculate the bouyancy using the probes
 @onready var probes = $ProbeContainer.get_children()
-#-----------------------------------------------------------------
 var submerged := false
-
-const water_height := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -46,7 +40,7 @@ func _physics_process(_delta):
 		var depth = water.get_height(p.global_position) - p.global_position.y
 		if depth > 0:
 			submerged = true
-			apply_central_force(Vector3.UP * buoyancy_force)
+			apply_force(Vector3.UP * buoyancy_force, p.global_position - global_position)
 
 func _integrate_forces(state:PhysicsDirectBodyState3D):
 	
