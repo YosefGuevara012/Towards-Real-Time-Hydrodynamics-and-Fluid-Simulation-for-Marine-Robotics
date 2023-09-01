20d002da0d365e4894cf359e3ad79979b7636ccc
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2023-08-31 23:39:50 -0500

    Collider of the nomad drifter to facilitate the calculation of the volume of the object

--- Scenes/nomad_filter.gd
+++ Scenes/nomad_filter.gd
@@ -1,8 +1,10 @@
 extends RigidBody3D
 
+
 @export var float_force := 1.0
 @export var water_drag := 0.05
 @export var water_angular_drag := 0.05
+@export_range(1020, 1029) var surface_seawater_density : int =  1020
 
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var water = get_node('/root/World/WaterPlane')
@@ -24,6 +26,7 @@ func _ready():
 #	pass
 
 func _physics_process(_delta):
+	print(global_position.y)
 	submerged = false
 	
 	#----------------------Code without probes-----------------------------
@@ -38,7 +41,7 @@ func _physics_process(_delta):
 		var depth = water.get_height(p.global_position) - p.global_position.y 
 		if depth > 0:
 			submerged = true
-			apply_force(Vector3.UP * float_force * gravity * depth, p.global_position - global_position)
+			apply_force(Vector3.UP * surface_seawater_density * float_force * gravity , p.global_position - global_position)
 
 func _integrate_forces(state:PhysicsDirectBodyState3D):
 	
